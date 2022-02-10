95d767751c86dc979aaaf409249716a758f6308b
Clemens Schmid <clemens@nevrome.de>
2022-02-10 18:49:54 +0100

    playing around with some visual options

--- .github/website_generator/generate_source.R
+++ .github/website_generator/generate_source.R
@@ -2,6 +2,7 @@
 total_janno <- poseidonR::read_janno("../../.", validate = F)
 
 pac_names <- total_janno$source_file |> unique() |> dirname()
+pac_names <- pac_names[1:3]
 
 generate_Rmd <- function(x) {
   path <- file.path("website_source", paste0(x, ".Rmd"))
@@ -14,13 +15,17 @@ purrr::walk(pac_names, generate_Rmd)
 
 index_file <- file.path("website_source", "index.Rmd")
 
-write(
-  purrr::map_chr(
+pac_df <- data.frame(
+  pac_links = purrr::map_chr(
     pac_names,
     function(x) {
       paste0("[", x, "](", x, ".html)")
     }
-  ),
+  )
+)
+
+write(
+  knitr::kable(pac_df, format = "pipe") %>% as.character(),
   file = index_file
 )
 
