35d28112d7c0eb5803e47c8a13b28e03b01cabaa
Clemens Schmid <clemens@nevrome.de>
2022-02-11 15:40:20 +0100

    added new counts and a link to the overview table

--- website_generator/generate_source.R
+++ website_generator/generate_source.R
@@ -5,16 +5,22 @@ total_janno <- poseidonR::read_janno("../.", validate = F)
 janno_table <- total_janno %>%
   dplyr::group_by(source_file) %>%
   dplyr::summarise(
-    `# Individuals` = dplyr::n(),
+    `# Inds` = dplyr::n(),
     Package = dirname(unique(source_file)),
-    `View link` = paste0("[View](", Package, ".html)"),
+    `# ancient` = (Date_Type %in% c("C14", "context")) %>% sum(na.rm = TRUE),
+    `# modern` = (Date_Type == "modern") %>% sum(na.rm = TRUE),
+    `Github link` = paste0(
+      "[Github](https://github.com/poseidon-framework/published_data/tree/master/",
+      Package, ")"
+    ),
     `Download link` = paste0(
       "[Download](http://c107-224.cloud.gwdg.de:3000/zip_file/", Package, ")"
-    )
+    ),
+    `View link` = paste0("[View](", Package, ".html)")
   ) %>%
   dplyr::select(-source_file)
 
-pac_names <- janno_table$Package
+pac_names <- janno_table$Package#[1:2]
 
 generate_Rmd <- function(x) {
   path <- file.path("website_source", paste0(x, ".Rmd"))
