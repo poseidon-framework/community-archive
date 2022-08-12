8e158f1796a4fdf33023514714be2d78a34088ba
Clemens Schmid <clemens@nevrome.de>
2022-08-12 15:40:14 +0200

    cleaned the script a bit

--- checkForDisplacedFiles.sh
+++ checkForDisplacedFiles.sh
@@ -3,22 +3,21 @@
 findStatusFile=$(mktemp -t tmp.XXXXXXXXXX)
 
 for dir in */ ; do
-    if [[ "$dir" == "website_generator/" ]] || [[ "$dir" =~ ^\..* ]]; then
-        break
-    fi
-    echo Checking: "$dir"
-    find "$dir" -type f -not -name "POSEIDON.yml" \
-      -exec bash -c '
-        searchFile=$(basename "$1" .deb)
-        if ! grep -Fq $searchFile "$2POSEIDON.yml"; then
-           echo Not properly referenced in POSEIDON.yml file: "$1" \("$2POSEIDON.yml"\) >> "$3"
-        fi
-      ' -- {} "$dir" "$findStatusFile" \;
+  if [[ "$dir" == "website_generator/" ]] || [[ "$dir" =~ ^\..* ]]; then
+    break
+  fi
+  echo Checking: "$dir"
+  find "$dir" -type f -not -name "POSEIDON.yml" -exec bash -c '
+      searchFile=$(basename "$1")
+      if ! grep -Fq $searchFile "$2POSEIDON.yml"; then
+         echo Not properly referenced in POSEIDON.yml file: "$1" \(searched in "$2POSEIDON.yml"\) >> "$3"
+      fi
+    ' -- {} "$dir" "$findStatusFile" \;
 done
 
 echo ""
 if [ -s "$findStatusFile" ]; then
-  echo 1>&2 "One or multiple issues detected:"
+  echo One or multiple issues detected:
   cat "$findStatusFile"
   exit 1  
 else
