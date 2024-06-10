b9323790f2ba6b8e4db4176aab4295c18da579fa
Clemens Schmid <clemens@nevrome.de>
2024-06-10 10:36:06 +0200

    Update checkDoubleQuote.sh
    Co-authored-by: Thiseas C. Lamnidis <thisseass@gmail.com>

--- checkDoubleQuote.sh
+++ checkDoubleQuote.sh
@@ -2,7 +2,7 @@
 
 # function to check if a file starts with a double-quote
 starts_with_quote() {
-  if grep -q '^"' "$1"; then
+  if grep -q '^"' "${1}"; then
     return 0
   else
     return 1
@@ -18,11 +18,11 @@ exit_code=0
 # recursive search for files and check them
 for ext in "${extensions[@]}"; do
   while IFS= read -r -d '' file; do
-    if starts_with_quote "$file"; then
-      echo "FAIL: $file"
+    if starts_with_quote "${file}"; then
+      echo "FAIL: ${file}"
       exit_code=1
     fi
-  done < <(find . -type f -name "*.$ext" -not -path "./.git*" -print0)
+  done < <(find . -type f -name "*.${ext}" -not -path "./.git*" -print0)
 done
 
-exit $exit_code
+exit ${exit_code}
