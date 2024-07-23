0efbc41113d1888ea685247b192a37c4c3252a46
dhananjaya93 <dhananjaya.at@gmail.com>
2024-07-23 21:41:58 +0200

    script for .diff parsing

---
+++ Parsing_new_package_diff/parse_diff.py
@@ -0,0 +1,29 @@
+import re
+import sys
+
+"""Regex : ^ from the start of the line. 
+\+: matches + sign. \s*: matches whitespace between + and other text. - matches hypen. title: matches title. 
+(.*?): Matches package name. "\n:" matches newline. \+ matches plus sign. again (.*?) to capture version """
+
+
+def parse_diff(diff):
+    new_packages = []
+    # Regex to find lines with new packages
+    package_pattern = re.compile(r'^\+\s*-\s*title:\s*(.*?)\n\+\s*version:\s*(.*?)$', re.MULTILINE)
+
+    matches = package_pattern.findall(diff)
+    for match in matches:
+        title, version = match
+        new_packages.append(f"{title} (v{version})")
+
+    return new_packages
+
+if __name__ == "__main__":
+    diff = sys.stdin.read()
+    new_packages = parse_diff(diff)
+    if new_packages:
+        toot_content = "New packages in the community-archive:\n"
+        toot_content += "\n".join(new_packages)
+        toot_content += "\ngithub.com/poseidon-framework/community-archive #aDNA #OpenData"
+        print(toot_content)
+
